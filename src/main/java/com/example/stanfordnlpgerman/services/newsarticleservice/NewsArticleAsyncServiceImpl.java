108d6adb1a12d860fe9fe004deda3194a1939be4
Gilthanas122 <pityu.django@gmail.com>
2022-05-06 19:53:29 +0200

    bug fix in saving a newsarticle

--- src/main/java/com/example/stanfordnlpgerman/services/newsarticleservice/NewsArticleAsyncServiceImpl.java
+++ src/main/java/com/example/stanfordnlpgerman/services/newsarticleservice/NewsArticleAsyncServiceImpl.java
@@ -5,167 +5,143 @@ import com.example.stanfordnlpgerman.models.dao.*;
 import com.example.stanfordnlpgerman.models.dtos.newsarticle.CreateNewsPaperArticleDTO;
 import com.example.stanfordnlpgerman.repositories.NewsArticleRepository;
 import com.example.stanfordnlpgerman.services.lemmatypeservice.LemmaTypeService;
-import com.example.stanfordnlpgerman.services.texttokenservice.TextTokenService;
 import com.example.stanfordnlpgerman.services.validations.ErrorServiceImpl;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.CoreDocument;
 import edu.stanford.nlp.pipeline.CoreSentence;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
+@Slf4j
 public class NewsArticleAsyncServiceImpl implements NewsArticleAsyncService {
-  private final List<TextToken> filteredTextTokens = new ArrayList<>();
-  private final Set<TextToken> invalidTextTokens = new HashSet<>();
-  private final NewsArticleRepository newsArticleRepository;
-  private final LemmaTypeService lemmaTypeService;
-  private final StanfordCoreNLP pipeline;
-  private final TextTokenService textTokenService;
+    private final List<TextToken> filteredTextTokens = new ArrayList<>();
+    private final NewsArticleRepository newsArticleRepository;
+    private final LemmaTypeService lemmaTypeService;
+    private final StanfordCoreNLP pipeline;
 
-  public NewsArticleAsyncServiceImpl(NewsArticleRepository newsArticleRepository, LemmaTypeService lemmaTypeService, StanfordCoreNLP pipeline, TextTokenService textTokenService) {
-    this.newsArticleRepository = newsArticleRepository;
-    this.lemmaTypeService = lemmaTypeService;
-    this.pipeline = pipeline;
-    this.textTokenService = textTokenService;
-  }
-
-  @Override
-  public void saveNewsPaperArticles(List <CreateNewsPaperArticleDTO>  createNewsPaperArticleDTOS){
-    createNewsPaperArticleDTOS
-            .forEach(this::saveNewsPaperArticle);
-  }
-
-  @Async
-  public void saveNewsPaperArticle(CreateNewsPaperArticleDTO createNewsPaperArticleDTO)  {
-    long startTime = System.currentTimeMillis();
-    NewsArticle newsArticle = NewsArticle
-            .builder()
-            .newsPaperName(createNewsPaperArticleDTO.getNewsPaperName())
-            .title(createNewsPaperArticleDTO.getTitle())
-            .publicationYear(createNewsPaperArticleDTO.getPublicationYear())
-            .build();
-    newsArticle.setSentences(createSentencesFromNewsPaperArticle(createNewsPaperArticleDTO.getText(), newsArticle));
-    List<LemmaType> lemmaTypes = createLemmaTypesFromSentencesForNewsArticle(newsArticle.getSentences());
-    newsArticle.setLemmaTypes(lemmaTypes);
-    newsArticle.setRelevance(setRelevanceForNewsArticleByLemmaTypes(lemmaTypes));
-    newsArticleRepository.save(newsArticle);
-    textTokenService.saveAllInvalidTextTokens(invalidTextTokens);
-    long endTime = System.currentTimeMillis();
-    System.out.println("Time elapsed " + (endTime - startTime));
-  }
-
-  private int setRelevanceForNewsArticleByLemmaTypes(List<LemmaType> lemmaTypes) {
-    int relevance = 0;
-    Set<String> keyWords = KeyWordsSingleton.getKeyWords();
-    for (String keyword : keyWords) {
-      for (LemmaType lemmaType : lemmaTypes) {
-        if (lemmaType.getText().equals(keyword)){
-          relevance++;
-        }
-      }
+    public NewsArticleAsyncServiceImpl(NewsArticleRepository newsArticleRepository, LemmaTypeService lemmaTypeService, StanfordCoreNLP pipeline) {
+        this.newsArticleRepository = newsArticleRepository;
+        this.lemmaTypeService = lemmaTypeService;
+        this.pipeline = pipeline;
     }
-    return relevance;
-  }
 
-  private List<LemmaType> createLemmaTypesFromSentencesForNewsArticle(List<Sentence> sentences) {
-    List<LemmaType> lemmaTypes = new ArrayList<>();
-    for (Sentence sentence : sentences) {
-      lemmaTypes.addAll(sentence.getLemmaTypes());
-    }
-    return lemmaTypes;
-  }
-
-  private List<Sentence> createSentencesFromNewsPaperArticle(String text, NewsArticle newsArticle) {
-    CoreDocument coreDocument = new CoreDocument(text);
-    pipeline.annotate(coreDocument);
-    List<CoreSentence> coreSentences = coreDocument.sentences();
-    List<Sentence> sentences = new ArrayList<>();
-    short position = 0;
-    for (CoreSentence sent : coreSentences) {
-      Sentence sentence = Sentence
-              .builder()
-              .newsArticle(newsArticle)
-              .text(sent.text())
-              .textPosition(position)
-              .build();
-      if (ErrorServiceImpl.invalidSentence(sentence.getText())) {
-        sentence.setInvalid(true);
-      }
-      sentence.setLemmaTypes(createLemmaTypesFromSentences(sent, sentence, newsArticle));
-      sentence.setTextTokens(filteredTextTokens);
-      filteredTextTokens.clear();
-      sentences.add(sentence);
-      position++;
-    }
-    return sentences;
-  }
-
-  @Transactional
-  protected List<LemmaType> createLemmaTypesFromSentences(CoreSentence coreSentence, Sentence sentence, NewsArticle newsArticle) {
-    List<CoreLabel> coreLabels = coreSentence.tokens();
-    List<LemmaType> lemmaTypes = new ArrayList<>();
-    short position = 0;
-    short coreLabelPosition = 0;
-
-    for (String word : coreSentence.tokensAsStrings()) {
-      word = word.replaceAll("[^0-9\\p{L}\\s]", "");
-      if (!word.isEmpty()) {
-        String phraseType = coreLabels.get(coreLabelPosition).get(CoreAnnotations.PartOfSpeechAnnotation.class);
-        List<LemmaType> lemmaTypesReturned = lemmaTypeService.findByText(word);
-        TextToken textToken = TextToken
+    @Async
+    public void saveNewsPaperArticle(CreateNewsPaperArticleDTO createNewsPaperArticleDTO) {
+        NewsArticle newsArticle = NewsArticle
                 .builder()
-                .text(word)
-                .sentencePosition(position)
-                .sentence(sentence)
-                .phraseType(phraseType)
+                .newsPaperName(createNewsPaperArticleDTO.getNewsPaperName())
+                .title(createNewsPaperArticleDTO.getTitle())
+                .publicationYear(createNewsPaperArticleDTO.getPublicationYear())
                 .build();
-        filteredTextTokens.add(textToken);
-        if (lemmaTypesReturned.size() == 1) {
-          LemmaType lemmaType = getLemmaTypeFromList(word, lemmaTypesReturned);
-          lemmaType.addOneTextToken(textToken);
-          lemmaType.addOneSentence(sentence);
-          lemmaType.addOneNewsArticle(newsArticle);
-          textToken.setLemmaType(lemmaType);
-          lemmaTypes.add(lemmaType);
-        } else {
-          textToken.setInvalid(true);
-          invalidTextTokens.add(textToken);
-        }
-        position++;
-      }
-      coreLabelPosition++;
+        newsArticle.setSentences(createSentencesFromNewsPaperArticle(createNewsPaperArticleDTO.getText(), newsArticle));
+        Set<LemmaType> lemmaTypes = createLemmaTypesFromSentencesForNewsArticle(newsArticle.getSentences());
+        newsArticle.setLemmaTypes(lemmaTypes);
+        newsArticle.setRelevance(setRelevanceForNewsArticleByLemmaTypes(lemmaTypes));
+        log.info("NewsArticle with title {} and number of sentences {} saved", newsArticle.getTitle(), newsArticle.getSentences().size());
+        newsArticleRepository.save(newsArticle);
     }
-    return lemmaTypes;
-  }
 
-  private LemmaType getLemmaTypeFromList(String word, List<LemmaType> lemmaTypesReturned) {
-    LemmaType toReturn = null;
-    for (LemmaType lemmaType : lemmaTypesReturned) {
-      if (lemmaType.getText().equalsIgnoreCase(word)) {
-        toReturn = lemmaType;
-      } else {
-        toReturn = lemmaType.getLemmaTokens()
+    @Override
+    public void saveNewsPaperArticles(List<CreateNewsPaperArticleDTO> createNewsPaperArticleDTOS) {
+        createNewsPaperArticleDTOS.forEach(this::saveNewsPaperArticle);
+    }
+
+    private int setRelevanceForNewsArticleByLemmaTypes(Set<LemmaType> lemmaTypes) {
+        final int[] relevance = {0};
+        Set<String> keyWords = KeyWordsSingleton.getKeyWords();
+        lemmaTypes.forEach(lemmaType -> {
+            if (keyWords.contains(lemmaType.getText())) {
+                relevance[0] += 1;
+            }
+        });
+        return relevance[0];
+    }
+
+    private Set<LemmaType> createLemmaTypesFromSentencesForNewsArticle(List<Sentence> sentences) {
+        return sentences
                 .stream()
-                .filter(lemmaToken -> lemmaToken.getText().equalsIgnoreCase(word))
-                .findFirst()
-                .map(LemmaToken::getLemmaType)
-                .orElse(null);
-      }
+                .map(Sentence::getLemmaTypes)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
     }
-    if (toReturn == null) {
-      LemmaToken lemmaTokenToGetPhraseType = new LemmaToken();
-      for (LemmaType lt : lemmaTypesReturned) {
-        toReturn = lt;
-        lemmaTokenToGetPhraseType = (LemmaToken) lt.getLemmaTokens().stream().toArray()[0];
-      }
-      toReturn.addOneLemmaToken(LemmaToken.builder().text(word).lemmaType(toReturn).phraseType(lemmaTokenToGetPhraseType.getPhraseType()).build());
+
+    private List<Sentence> createSentencesFromNewsPaperArticle(String text, NewsArticle newsArticle) {
+        CoreDocument coreDocument = new CoreDocument(text);
+        pipeline.annotate(coreDocument);
+        List<CoreSentence> coreSentences = coreDocument.sentences();
+
+        short[] position = {0};
+        List<Sentence> sentences = new ArrayList<>();
+                coreSentences.forEach(coreSentence -> {
+                    Sentence sentence = Sentence
+                            .builder()
+                            .newsArticle(newsArticle)
+                            .text(coreSentence.text())
+                            .textPosition(position[0])
+                            .build();
+                    if (ErrorServiceImpl.invalidSentence(sentence.getText())) {
+                        sentence.setInvalid(true);
+                    }
+                    sentence.setLemmaTypes(createLemmaTypesFromSentences(coreSentence, sentence, newsArticle));
+                    sentence.setTextTokens(new ArrayList<>(filteredTextTokens));
+                    filteredTextTokens.clear();
+                    position[0]++;
+                    sentences.add(sentence);
+                });
+        return sentences;
     }
-    return toReturn;
-  }
+
+    @Transactional
+    protected Set<LemmaType> createLemmaTypesFromSentences(CoreSentence coreSentence, Sentence sentence, NewsArticle newsArticle) {
+        List<CoreLabel> coreLabels = coreSentence.tokens();
+        final short[] position = {0};
+        final short[] coreLabelPosition = {0};
+
+        Set<LemmaType> lemmaTypes = new HashSet<>();
+                coreSentence.tokensAsStrings().stream()
+                        .filter(word -> !word.replaceAll("[^0-9\\p{L}\\s]", "").isEmpty())
+                        .forEach(word -> {
+                            LemmaType lemmaType = null;
+                            String phraseType = coreLabels.get(coreLabelPosition[0]).get(CoreAnnotations.PartOfSpeechAnnotation.class);
+                            Set<LemmaType> lemmaTypesFromDatabase = lemmaTypeService.findAllByText(word);
+                            TextToken textToken = TextToken
+                                    .builder()
+                                    .text(word)
+                                    .sentencePosition(position[0])
+                                    .sentence(sentence)
+                                    .phraseType(phraseType)
+                                    .build();
+                            filteredTextTokens.add(textToken);
+                            if (lemmaTypesFromDatabase.size() == 1) {
+                                lemmaType = getLemmaTypeFromSet(word, lemmaTypesFromDatabase);
+                                lemmaType.addOneTextToken(textToken);
+                                lemmaType.addOneSentence(sentence);
+                                lemmaType.addOneNewsArticle(newsArticle);
+                                textToken.setLemmaType(lemmaType);
+                            } else {
+                                textToken.setInvalid(true);
+                            }
+                            position[0]++;
+                            coreLabelPosition[0]++;
+                            if (lemmaType != null){
+                                lemmaTypes.add(lemmaType);
+                            }
+                        });
+        return lemmaTypes;
+    }
+
+    private LemmaType getLemmaTypeFromSet(String word, Set<LemmaType> lemmaTypesFromDatabase) {
+           return lemmaTypesFromDatabase.stream()
+                   .reduce((first, second) -> first)
+                   .orElse(null);
+    }
 }
