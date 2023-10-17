caccd8f4f5156d6221ef5a0fcbab9f2562110aaa
Pityu Szivos <pityu.django@gmail.com>
2023-10-17 16:03:48 +0200

    LEM-6 refactor KeyWordsSingleton

--- src/main/java/com/example/stanfordnlpgerman/models/KeyWordsSingleton.java
+++ src/main/java/com/example/stanfordnlpgerman/models/KeyWordsSingleton.java
@@ -1,14 +1,18 @@
 package com.example.stanfordnlpgerman.models;
 
+import lombok.Getter;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class KeyWordsSingleton {
-  private static Set<String> keyWords =
+  @Getter
+  private final static Set<String> keyWords =
           new HashSet<>(Arrays.asList("Ungarndeutsch", "Wir", "Familie"));
 
-  public static Set<String> getKeyWords() {
-    return keyWords;
+  public static boolean  isKeyWord(String word) {
+    return keyWords.contains(word);
   }
+
 }
